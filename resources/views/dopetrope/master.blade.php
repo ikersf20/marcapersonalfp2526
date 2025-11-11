84eaad61222d8a7ad980e48a64e7ac2f06b66070
Iker <2095202@alu.murciaeduca.es>
2025-11-11 09:15:58 +0100

    Vista home integrada en dopetre

--- resources/views/dopetrope/master.blade.php
+++ resources/views/dopetrope/master.blade.php
@@ -29,6 +29,7 @@
 					<!-- Nav -->
 						<nav id="nav">
 							<ul>
+                                @section('menu')
 								<li class="current"><a href="index.html">Home</a></li>
 								<li>
 									<a href="#">Dropdown</a>
@@ -51,15 +52,15 @@
 								</li>
 								<li><a href="left-sidebar.html">Left Sidebar</a></li>
 								<li><a href="right-sidebar.html">Right Sidebar</a></li>
-								<li><a href="no-sidebar.html">No Sidebar</a></li>
-							</ul>
+                                <li><a href="no-sidebar.html">No Sidebar</a></li>
+                                @show
+                            </ul>
 						</nav>
 
 					<!-- Banner -->
 						<section id="banner">
 							<header>
-								<h2>Howdy. This is Dopetrope.</h2>
-								<p>A responsive template by HTML5 UP</p>
+								@yield('content')
 							</header>
 						</section>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,16 @@
 
 @section('menu')
     @parent
-    <p>¡Hola {{$nombre ?? 'colega'}}!</p>
+    <li>Opción adicional</li>
 @endsection
 
 
 
 
 @section('content')
-    <ul>
-    @if( count($users) === 1 )
-        <li>Solo hay un usuario!</li>
-    @elseif (count($users) > 1)
-        <li>Hay muchos usuarios!</li>
-        @include('users.usersList', ['users' => $users])
-    @else
-        <li>No hay ningún usuario :(</li>
-    @endif
-    </ul>
+
+    <h2>Marca Personal F.P.</h2>
+	<p>Página principal</p>
+
 @endsection
 

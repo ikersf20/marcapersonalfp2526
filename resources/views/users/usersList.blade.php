aafac2e168adca0d17ad799ac0b51c2169e4b4ab
Iker <2095202@alu.murciaeduca.es>
2025-11-11 09:43:31 +0100

    Varias vistas con dopetrope

--- resources/views/users/usersList.blade.php
+++ resources/views/users/usersList.blade.php
@@ -1,5 +1,16 @@
-<ul>
+@extends('dopetrope.master')
+
+
+
+@section('content')
+
+    <ul>
 @foreach ($users as $user)
     <li>Usuario {{ $user['name'] }} con identificador: {{ $user['id'] }}</li>
 @endforeach
 </ul>
+
+@endsection
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,11 @@ Route::get('/', function () {
     return view('home');
 });
 
-
+Route::get('/users', function () {
+    return view('users.usersList', [
+        'users' => [
+            ['id' => 1, 'name' => 'Ana' ],
+            ['id' => 2, 'name' => 'Luis' ],
+            ['id' => 3, 'name' => 'Carlos']]
+]);
+});

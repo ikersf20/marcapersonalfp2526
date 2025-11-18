6fac5dcba6498f098e8b38247c732bd5f6f40911
Iker <2095202@alu.murciaeduca.es>
2025-11-18 08:19:13 +0100

    a

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,41 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Pantalla principal';
 });
+
+// ----------------------------------------
+Route::get('login', function () {
+    return "Login usuario";
+});
+Route::get('logout', function () {
+    return "Logout usuario";
+});
+
+
+// ----------------------------------------
+Route::prefix('familias-profesionales')->group(function () {
+    Route::get('/', function () {
+        return 'Listado familias profesionales';
+    });
+
+    Route::get('/create', function () {
+        return 'Añadir familia profesional';
+    });
+
+    Route::get('/show/{id}	', function ($id) {
+        return 'Vista detalle familia profesional ' . $id;
+    }) -> where('id', '[0-9]+');
+
+    Route::get('/edit/{id}', function ($id) {
+        return 'Modificar familia profesional ' . $id;
+    }) -> where('id', '[0-9]+');
+});
+
+
+// ----------------------------------------
+Route::get('perfil/{id?}', function ($id = null) {
+    if ($id === null)
+        return 'Visualizar el usuario propio';
+    return 'Visualizar el usuario de ' . $id;
+}) -> where('id', '[0-9]+');

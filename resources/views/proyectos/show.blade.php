c6d22968124e0aa82840544d90de44d3681e478e
Iker <2095202@alu.murciaeduca.es>
2025-11-19 09:51:58 +0100

    Ejercicios controladores

--- resources/views/proyectos/show.blade.php
+++ resources/views/proyectos/show.blade.php
@@ -4,5 +4,41 @@
 
     Vista detalle proyecto {{$id}}
 
+
+    <div class="row m-4">
+
+        <div class="col-sm-4">
+
+            <a href="#" class="image featured"><img src="{{ asset('/images/mp-logo.png') }}" alt="" style="width: 100px" /></a>
+
+        </div>
+        <div class="col-sm-8">
+
+            {{-- TODO: Datos del proyecto --}}
+            <strong>Dominio: </strong>{{$proyecto['dominio']}} <br>
+            <strong>Nombre: </strong>{{$proyecto['nombre']}} <br>
+            <strong>Fecha de inicio: </strong>{{$proyecto['metadatos']['fecha_inicio']}} <br>
+            <strong>Fecha de fin: </strong>{{$proyecto['metadatos']['fecha_fin']}} <br>
+            <strong>Calificación: </strong>{{$proyecto['metadatos']['calificacion']}} <br>
+
+            @if ($proyecto['metadatos']['calificacion'] >= 5)
+                <strong>Estado: </strong>Proyecto probado <br><br>
+
+                <button style="background-color: red">Suspender proyecto</button>
+
+
+            @else
+                <strong>Estado: </strong>Proyecto suspenso<br><br>
+                <button style="background-color: aquamarine">Aprobar proyecto</button>
+
+            @endif
+            <br><br>
+
+            <a href="{{ action([App\Http\Controllers\ProyectosController::class, 'getEdit'],['id' => $id]) }}" class="button alt">Editar proyecto</a>
+
+            <a href="{{ action([App\Http\Controllers\ProyectosController::class, 'getIndex']) }}" class="button alt">Volver al listado</a>
+        </div>
+    </div>
+
 @stop
 
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProyectosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-
-    return view('home');
-
-});
+Route::get('/', [HomeController::class, 'getHome']);
 
 // ----------------------------------------
 Route::get('login', function () {
@@ -39,21 +37,13 @@ Route::prefix('familias-profesionales')->group(function () {
     }) -> where('id', '[0-9]+');
 });
 Route::prefix('proyectos')->group(function () {
-    Route::get('/', function () {
-        return view('proyectos.index');
-    });
+    Route::get('/', [ProyectosController::class, 'getIndex']);
 
-    Route::get('create', function () {
-        return view('proyectos.create');
-    });
+    Route::get('create', [ProyectosController::class, 'getCreate']);
 
-    Route::get('/show/{id}', function ($id) {
-        return view('proyectos.show', array('id'=>$id));
-    }) -> where('id', '[0-9]+');
+    Route::get('/show/{id}', [ProyectosController::class, 'getShow']) -> where('id', '[0-9]+');
 
-    Route::get('/edit/{id}', function ($id) {
-        return view('proyectos.edit', array('id'=>$id));
-    }) -> where('id', '[0-9]+');
+    Route::get('/edit/{id}', [ProyectosController::class, 'getEdit']) -> where('id', '[0-9]+');
 });
 
 
